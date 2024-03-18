f038caded1b9b2b117e38b425af1eb3418cb638e
samuelaraujo <samuelbrazdearaujo@hotmail.com>
2024-03-17 23:11:02 -0500

    Commit Inicial do Projeto

---
+++ src/main/java/com/projeto/projetopje/ProjetopjeApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.projetopje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetopjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetopjeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/projetopje/ProjetopjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.projetopje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetopjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
